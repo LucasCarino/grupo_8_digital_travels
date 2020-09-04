f31ae5e6454a78c7a2caa84d266905b8f4564dc3
sanaranda <santiagoaranda@gmail.com>
2020-09-04 20:37:21 -0300

    agregado cableado para paginas pedidas

--- app.js
+++ app.js
@@ -9,9 +9,18 @@ app.get('/', function (req, res){
     res.sendFile(__dirname + '/public/html/index.html');
 })
 app.get('/registro', function (req, res){
-    res.sendFile(__dirname + '/register.html');
+    res.sendFile(__dirname + '/public/html/register.html');
+})
+app.get('/productos', function (req, res){
+    res.sendFile(__dirname + '/public/html/productDetail.html');
+})
+app.get('/carrito', function (req, res){
+    res.sendFile(__dirname + '/public/html/productCart.html');
+})
+app.get('/login', function (req, res){
+    res.sendFile(__dirname + '/public/html/login.html');
 })
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico')); //para icono en barra 
