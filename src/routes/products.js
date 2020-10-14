26d81b11fee12c7e2664bce3b9e8cfddc253dbba
Lucas <lucascarino2@gmail.com>
2020-10-14 15:59:47 -0300

    pagina detail agregada

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
     res.render('products/products', { title: 'Productos' });
+});
+
+router.get('/detail', (req, res, next) => {
+    res.render('products/productDetail', { title: 'Producto 1' });
 });
 
 module.exports = router;
