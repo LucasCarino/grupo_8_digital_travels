8a473b033e3c0b931dd60fcce6ca2be19444ec01
santiperone <sdperone97@gmail.com>
2020-10-15 10:35:48 -0300

    formularios de carga y edición de productos

--- src/routes/products.js
+++ src/routes/products.js
@@ -8,5 +8,11 @@ router.get('/', (req, res, next) => {
 router.get('/detail', (req, res, next) => {
     res.render('products/productDetail', { title: 'Producto 1' });
 });
+router.get('/create', (req, res, next) => {
+    res.render('products/createProduct', { title: 'Vender' });
+});
+router.get('/edit/:id', (req, res, next) => {
+    res.render('products/editProduct', { title: 'Editar' });
+});
 
 module.exports = router;
